a59473f363c6a5e12c68310d608705f7d053f2c9
Arshdeep Singh <11322brar@gmail.com>
2025-03-19 05:01:22 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import {
+  Book,
+  Briefcase,
+  Code,
+  FileText,
+  Folder,
+  Home,
+  Mail,
+  User,
+} from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import { ModeToggle } from "./components/mode-toggle";
+
+function App() {
+  const dockItems = [
+    { title: "Home", icon: <Home />, href: "/" },
+    { title: "About Me", icon: <User />, href: "/about" },
+    { title: "Skills", icon: <Code />, href: "/skills" },
+    { title: "Projects", icon: <Folder />, href: "/projects" },
+    { title: "Experience", icon: <Briefcase />, href: "/experience" },
+    { title: "Blog", icon: <Book />, href: "/blog" },
+    { title: "Resume", icon: <FileText />, href: "/resume" },
+    { title: "Contact", icon: <Mail />, href: "/contact" },
+  ];
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+      <FloatingDock items={dockItems} desktopClassName="bottom-0 absolute " mobileClassName="" />
+      <div className="absolute top-4 right-4">
+        <ModeToggle />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import "./index.css";
+
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
+import { ThemeProvider } from "@/components/theme-provider";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </StrictMode>,
+);
